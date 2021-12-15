53564c4bbae089accdc9bb2b90bd9d79ae6d0efa
ikhana <inaamullah484@gmail.com>
2021-12-16 01:16:36 +0500

    issues fixed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
     })
     $(document).on('keypress',function(e){
         
-        if(e.which == 119){
+        if(e.which == 49){
             if(light){
                 light=false;
                 $img.attr("src",cars[0]);
